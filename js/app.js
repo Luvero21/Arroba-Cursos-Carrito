a1bb46e1d01900d30cb6177f93dca3e58137d142
luvero <luciana_lr@hotmail.com>
2022-05-14 01:12:34 -0300

    arroba

---
+++ js/app.js
@@ -0,0 +1,113 @@
+//VARIABLES
+const carrito = document.querySelector('#carrito');
+
+const listaCursos = document.querySelector('#lista-cursos');
+
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+let articulosCarrito = [];
+
+// Listeners
+
+cargarEventListeners();
+ function cargarEventListeners (){
+listaCursos.addEventListener('click',agregarCurso);
+
+carrito.addEventListener('click', eliminarCurso);
+
+vaciarCarritoBtn.addEventListener('click',()=>{
+     articulosCarrito=[];
+     limpiarHtml();
+})
+ };
+
+ function agregarCurso (e){
+      e.preventDefault();
+
+      if(e.target.classList.contains('agregar-carrito')){
+          const cursoSeleccionado = e.target.parentElement.parentElement;
+          leerDatosCurso(cursoSeleccionado);
+      }
+ };
+ function eliminarCurso(e){
+      if(e.target.classList.contains('borrar-curso')){
+           const cursoId = e.target.getAttribute('date-id');
+
+           articulosCarrito=articulosCarrito.filter(curso => curso.id !== cursoId);
+
+           carritoHTML();
+
+      }
+
+ }
+function leerDatosCurso(curso){
+     const infoCurso={
+          imagen:curso.querySelector('img').src,
+          titulo:curso.querySelector('h4').textContent,
+          precio:curso.querySelector('.precio span').textContent,
+          id:curso.querySelector('a').getAttribute('data-id'),
+          cantidad:1,
+
+     }
+
+     const existe= articulosCarrito.some(curso =>curso.id===infoCurso.id);
+     if(existe){
+
+          const curso = articulosCarrito.map(curso => {
+
+               if(curso.id===infoCurso.id){
+                    curso.cantidad++;
+                    return curso;
+
+               }else{
+                    return curso;
+               }
+          });
+          articulosCarrito=[...curso]
+     }else{
+          articulosCarrito= [...articulosCarrito, infoCurso];
+
+     }
+
+carritoHTML();
+}
+
+function carritoHTML(){
+limpiarHtml();
+
+     articulosCarrito.forEach(curso =>{
+          const {imagen, titulo,precio,cantidad, id} = curso
+          const row = document.createElement('tr')
+          row.innerHTML=`
+          <td>
+          <img src="${imagen}" width="100">
+          </td> 
+          <td>
+          ${titulo}
+          </td> 
+          <td>
+          ${precio}
+          </td> 
+          <td>
+          ${cantidad}
+          </td> 
+          <td>
+          <a href="#" class="borrar-curso" data-id="${id}"> X</a>
+          </td> 
+          `;
+          contenedorCarrito.appendChild(row)
+
+     });
+}
+
+function limpiarHtml(){
+    // contenedorCarrito.innerHTML='';
+while(contenedorCarrito.firstChild){
+     contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+}
+}
+
+
+
